ddcbf313ae26b7dea3024b9ea9aaf903cbbadc36
Clarissa <clarermason@gmail.com>
2026-09-25 11:07:37 -0700

    adding mobile menu dropdown

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,3 +2,17 @@
 document
   .getElementById("copyright")
   .appendChild(document.createTextNode(new Date().getFullYear()));
+
+// Hides/show moblie menu bar when needed
+var mobileMenuBtn = document.querySelector("#mobile-menu-btn");
+var mobileMenu = document.querySelector(".mobile-menu");
+mobileMenuBtn.addEventListener("click", () => {
+  console.log(mobileMenu.style.display);
+  if (mobileMenu.style.display === "none") {
+    console.log("hello");
+    mobileMenu.style.display = "flex";
+  } else {
+    console.log("goodbye");
+    mobileMenu.style.display = "none";
+  }
+});
